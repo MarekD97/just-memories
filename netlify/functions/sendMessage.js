357c97b2b7f3b849007b2a0a432cfb7d907b7485
MarekD97 <mdorosz2@wp.pl>
2022-07-03 19:42:52 +0200

    Fix sendMessage function

--- netlify/functions/sendMessage.js
+++ netlify/functions/sendMessage.js
@@ -6,19 +6,58 @@ const handlebars = require("handlebars");
 module.exports.handler = async function (event, context) {
   const req = JSON.parse(event.body);
 
-  const filePath = path.resolve(__dirname, "../../static/email-template.html");
-  const source = fs.readFileSync(filePath, "utf-8").toString();
-  const template = handlebars.compile(source);
-
-  console.log(filePath);
-
-  const replacements = {
-    name: req.name,
-    email: req.email,
-    message: req.message,
-    date: Date.now(),
-  };
-  const htmlToSend = template(replacements);
+  const template = `<!DOCTYPE html>
+  <html lang="pl">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Email template</title>
+      <style>
+          @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap");
+          html,
+          body,
+          form,
+          fieldset,
+          table,
+          tr,
+          td,
+          img {
+              margin: 0;
+              padding: 0;
+              font-family: "Lato", sans-serif;
+          }
+          body {
+              font-size: 1em;
+          }
+          .logo {
+              width: 130px;
+              height: 130px;
+          }
+          .row {
+              padding: 2em 1.5em;
+          }
+          .content {
+              background-color: #f4f4f4;
+              font-size: 1.25rem;
+          }
+          .email {
+              color: #000000;
+          }
+      </style>
+  </head>
+  <body>
+      <div>
+          <div class="row">
+              <h1>Wiadomość od: ${req.name}</h1>
+              <a class="email" href="mailto:${req.email}">Odpowiedz na adres: ${req.email}</a>
+          </div>
+          <div class="row content">
+              <p>${req.message}</p>
+          </div>
+      </div>
+  </body>
+  </html>`;
 
   const transporter = nodemailer.createTransport({
     host: "smtp.zoho.eu",
@@ -41,7 +80,7 @@ module.exports.handler = async function (event, context) {
       to: `"Just Memories" <${process.env.AUTH_USER}>`,
       replyTo: req.email,
       subject: "Wiadomość ze strony justmemoriesphoto.pl",
-      html: htmlToSend,
+      html: template,
     })
     .then((info) => (res.status = info.accepted ? "sent" : "error"))
     .catch((error) => {
